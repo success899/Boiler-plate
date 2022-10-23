763c3f8af25474f0b59d46eedfb36a4b407ae5e1
success899 <success899@naver.com>
2022-10-24 03:28:37 +0900

    BodyParser & Sign Up test

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser');
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
+
+const { User } = require('./models/User');
+
 const config = require('./config/key');
 
 const mongoose = require('mongoose')
@@ -12,6 +20,20 @@ mongoose.connect(config.mongoURI,{
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    
+    //회원가입 정보 client에서 가져오면 데이터베이스에 넣어주기
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+
 })
 
 app.listen(port, () => {
