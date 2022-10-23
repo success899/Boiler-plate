33f304b25ebd5dcf797884b4a785e46d10540f0e
success899 <success899@naver.com>
2022-10-24 02:20:41 +0900

    connect MongoDB

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const config = require('./config/key');
+
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI,{
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
