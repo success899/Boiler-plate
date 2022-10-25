b3067b6ee6f42b3b064d64298b11b988b4c31b09
success899 <success899@naver.com>
2022-10-25 21:31:17 +0900

    install proxy

--- server/index.js
+++ server/index.js
@@ -25,6 +25,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/api/hello', (req, res) => res.send('안녕하세요'))
+
 app.post('/api/users/register', (req, res) => {
     
     //회원가입 정보 client에서 가져오면 데이터베이스에 넣어주기
