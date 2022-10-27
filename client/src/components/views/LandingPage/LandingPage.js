1d190ecaca7d77befa9faccb3d88a3a568a4df92
success899 <success899@naver.com>
2022-10-27 18:44:44 +0900

    create Logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -8,9 +8,24 @@ function LandingPage() {
     .then(response => { console.log(response) })
     }, [])
 
+    const onClickHandler = () => {
+      axios.get('/api/users/logout')
+      .then(response =>{
+        if(response.data.success){
+          document.location.href="/login"
+        } else {
+          alert('로그아웃 실패.!')
+        }
+      })
+    }
+
   return (
     <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'}}>
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>
+        Logout
+      </button>
   </div>
   )
 }
