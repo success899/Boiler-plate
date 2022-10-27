6afbfe2d3c412cf0e9d14f2f11de6f4d99acf38e
success899 <success899@naver.com>
2022-10-27 18:22:11 +0900

    create Register

--- client/src/index.js
+++ client/src/index.js
@@ -9,21 +9,6 @@ import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
 import Reducer from './_reducers';
 
-// import 'antd/dist/antd.css';
-
-// const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//     <Provider>
-//         store={createStoreWithMiddleware(Reducer,
-//             window.__REDUX_DEVTOOLS_EXTENSION__ &&
-//             window.__REDUX_DEVTOOLS_EXTENSION__()
-//             )}
-//     <App />
-//     </Provider>
-// );
-
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
